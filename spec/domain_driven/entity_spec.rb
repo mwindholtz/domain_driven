d2d06e7a05996ed85e05d084eba60bdc4a1ba03b
Mark Windholtz <mark@agiledna.com>
2014-03-10 13:59:59 -0400

    ported Entity Spec (file)

---
+++ spec/domain_driven/entity_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+class EntityUnderTest < DomainDriven::Entity
+end
+  
+describe 'Entity' do
+
+  Given! (:model)   { double(:course_type => 'A', 'entity?'.to_sym => false) }  
+  Given! (:entity)  { EntityUnderTest.new(model) }
+  
+  context 'has model as _data ' do  
+    Then { entity.entity?.should == true }    
+    Then { entity._data.entity?.should == false }    
+    Then { entity._data.should == model}    
+  end 
+
+  context 'same as  _data ' do  
+    Then { entity._data == entity}    
+  end 
+
+  context 'same as self' do  
+    Then { entity == entity}    
+  end 
+
+  context 'class name' do 
+    context 'wrap same class as model' do 
+      Then { entity.class.to_s == "RSpec::Mocks::Mock" }  
+    end 
+
+    context 'wrap nil is still nil' do  
+      When(:entity)  { EntityUnderTest.wrap(nil)  }
+      Then { entity.class.to_s == "NilClass"}   
+    end 
+
+    context 'wraps nil is still nil' do  
+      When(:entity_list)  { EntityUnderTest.wraps(nil)  }
+      Then { entity_list.class.to_s == "NilClass"}   
+    end 
+
+    context 'class wraps a set of models' do  
+      When(:entity_list)  { EntityUnderTest.wraps( [model, model])  }
+      Then { entity_list.class.to_s == "Array"}   
+    end 
+  end 
+  
+  def self.wrap(entity)
+    entity ? new(entity) : nil
+  end
+  
+
+end
