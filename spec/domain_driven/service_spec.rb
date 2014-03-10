147c5eec205c7b21295a29403ed244647ca680ad
Mark Windholtz <mark@agiledna.com>
2014-03-10 18:42:39 -0400

    more service specs

--- spec/domain_driven/service_spec.rb
+++ spec/domain_driven/service_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
+require 'logger'
 
 class FakeService < DomainDriven::Service
   def perform_main
+    context[:course] = 'Its a horse-course of course'
   end
   
 end
   
 describe 'Service' do
 
-  When(:result) { FakeService.perform({}) }
-  Then { result }
+  When(:result) { FakeService.perform(request: {id: 123},  current_member: 'a-user', page_param: 1, logger: Logger.new(STDOUT)) }
+  Then { result.context[:course] == 'Its a horse-course of course' }
+  Then { result.context[:request][:id] == 123 }
 
 end
