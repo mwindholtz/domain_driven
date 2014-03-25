e8ed3fa11544156aa3290052e2dea392f9dd8223
Mark Windholtz <mark@agiledna.com>
2014-03-25 15:38:01 -0400

    fixed spec for RepositoryUnderTest

--- spec/domain_driven/repository_spec.rb
+++ spec/domain_driven/repository_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
+
+class RepositoryUnderTest  
+  include DomainDriven::Repository
+end
   
 describe 'Repository' do
 
-  Given! (:repository)   { DomainDriven::Repository.new(:fake_repo)  }  
+  Given! (:repository)   { RepositoryUnderTest.new(:fake_repo)  }  
 
   Then { repository.to_s == 'fake_repo'}  
 end
