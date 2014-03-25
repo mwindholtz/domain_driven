dcdd1c965d0643bf8e8261f59b2af7d77bb43302
Mark Windholtz <mark@agiledna.com>
2014-03-25 12:15:48 -0400

    make  DomainDriven::Repository an includable Module

--- lib/domain_driven/repository.rb
+++ lib/domain_driven/repository.rb
@@ -1,15 +1,24 @@
 require 'logger'
 
-class DomainDriven::Repository
-  attr_accessor :logger
-  def to_s
-    "#{@name}"
-  end
-
-  protected
-    def initialize(name)
-      @name   = name
-      self.logger = Logger.new(STDOUT)
+module DomainDriven
+  module Repository
+    attr_accessor :logger
+    def to_s
+      "#{@name}"
     end
-                    
+ 
+    module ClassMethods
+    end
+  
+    protected
+      def initialize(name)
+        @name   = name
+        self.logger = Logger.new(STDOUT)
+      end
+
+      def self.included(klass)
+        super
+        klass.extend ClassMethods
+      end
+  end
 end
